e86457ec84ba076b935bee5919d45d1afe63fad6
Siravit Pichphol <mai_za_2010@hotmail.com>
2021-07-09 19:59:41 +0700

    update countdata

--- app/src/main/java/com/maiguy/dessert/activity/edit_profile/view/EditProfileActivity.kt
+++ app/src/main/java/com/maiguy/dessert/activity/edit_profile/view/EditProfileActivity.kt
@@ -532,7 +532,7 @@ open class EditProfileActivity : AppCompatActivity(), View.OnClickListener {
                     if (faces.isNotEmpty()) {
                         NSFWDetector.isNSFW(bitmap) { isNSFW, confidence, image ->
                             if (isNSFW) {
-                                Snackbar.make(image1, "โรคจิต", Snackbar.LENGTH_SHORT).show()
+                                Snackbar.make(image1, (R.string.pornography), Snackbar.LENGTH_SHORT).show()
                                 backgroundAdd()
                                 dialog.dismiss()
                             } else {
@@ -589,7 +589,7 @@ open class EditProfileActivity : AppCompatActivity(), View.OnClickListener {
 
                     } else {
                         backgroundAdd()
-                        Snackbar.make(image1, "ไม่ใช่หน้าอีดอก", Snackbar.LENGTH_SHORT).show()
+                        Snackbar.make(image1, getString(R.string.recommend_profile), Snackbar.LENGTH_SHORT).show()
                     }
                 }
                 .addOnFailureListener { }
